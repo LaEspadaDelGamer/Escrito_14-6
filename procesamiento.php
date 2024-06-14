084c7a4055c50e92795ec85dd2adc141a50b56ac
Espadita <alumno@isbo.edu.uy>
2024-06-14 08:48:17 -0300

    agregamos la funcion mostrarProductos,la cual funciona sin problemas

--- procesamiento.php
+++ procesamiento.php
@@ -2,8 +2,8 @@
 session_start();
 
 // Definir las funciones
-//funcion para agregar productos a la lista
 
+//funcion para agregar productos a la lista
 function agregarProducto($Nombre, $Cantidad, $Valor, $Modelo) {
     $artefacto[] = [
         'Nombre' => $Nombre,
@@ -14,6 +14,7 @@ function agregarProducto($Nombre, $Cantidad, $Valor, $Modelo) {
     return $artefacto;
 }
 
+// hacemos una funcion para buscar el producto segun el modelo
 function buscarProductoPorModelo($artefacto, $Modelo) {
     foreach ($artefacto as $artefactos) {
         if ($artefactos['Modelo'] == $Modelo) {
@@ -22,4 +23,15 @@ function buscarProductoPorModelo($artefacto, $Modelo) {
     }
     return "Modelo no encontrado.<br>";
 }
+
+// hacemos una funcion que muestre todos los productos
+function mostrarProductos($artefacto) {
+    $result = '';
+    foreach ($artefacto as $artefactos) {
+        $result .= "Nombre: " . $artefactos['Nombre'] . ", Cantidad: " . $artefactos['Cantidad'] . " Valor: ". $artefactos['Valor']. " Modelo: " . $artefactos['Modelo'] . "<br>";
+
+    }
+    return $result;
+}
+
 ?>
--- prueba.php
+++ prueba.php
@@ -25,14 +25,25 @@ function buscarProductoPorModelo($artefacto, $Modelo) {
     }
     return "Modelo no encontrado.<br>";
 }
+//var_dump (buscarProductoPorModelo($artefacto, $Modelo));
+//funciona perfectamente
 
+// hacemos una funcion que muestre todos los productos
+function mostrarProductos($artefacto) {
+    $result = '';
+    foreach ($artefacto as $artefactos) {
+        $result .= "Nombre: " . $artefactos['Nombre'] . ", Cantidad: " . $artefactos['Cantidad'] . " Valor: ". $artefactos['Valor']. " Modelo: " . $artefactos['Modelo'] . "<br>";
 
+    }
+    return $result;
+}
+//var_dump (mostrarProductos($artefacto));
+//funciona perfectamente
 
 $Nombre = "algo";
 $Cantidad = 41;
 $Valor = 1500;
 $Modelo = "LG";
 $artefacto = agregarProducto($Nombre, $Cantidad, $Valor, $Modelo);
-//var_dump (buscarProductoPorModelo($artefacto, $Modelo));
-//funciona perfectamente
+
 ?>
